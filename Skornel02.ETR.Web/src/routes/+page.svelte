56989f5171db7eaca2ce07ff093e0c48a836cfe9
Kornél <stefankornel02@google.com>
2023-09-30 18:50:21 +0200

    Basest

--- Skornel02.ETR.Web/src/routes/+page.svelte
+++ Skornel02.ETR.Web/src/routes/+page.svelte
@@ -1,10 +1,14 @@
+<script>
+	import { base } from "$app/paths";
+
+</script>
 <main>
 	<h2><b>E</b>ntergalaktikus <b>T</b>anulmányi <b>R</b>endszer</h2>
 	<article class="container">
 		<div id="spacer" />
 		<div id="buttons">
-			<a href="hallgato/login" class="paper-btn"> Hallgatók / Students </a>
-			<a href="oktato/login" class="paper-btn"> Oktatók / Teachers </a>
+			<a href={`${base}/hallgato/login`} class="paper-btn"> Hallgatók / Students </a>
+			<a href={`${base}/oktato/login`} class="paper-btn"> Oktatók / Teachers </a>
 		</div>
 	</article>
 </main>
