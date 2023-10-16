228f5da12c109d8defcb66c16dcc2474a35a6b49
skornel02 <stefankornel02@gmail.com>
2023-10-16 18:27:19 +0000

    Deploying to gh-pages from  @ a03f8af60f5f574197e7be7025eae0a879afc97d 🚀

---
+++ _app/immutable/chunks/paths.5b80a862.js
@@ -0,0 +1,1 @@
+var s;const l=((s=globalThis.__sveltekit_pll7zu)==null?void 0:s.base)??"/szte-IB501g-2023-1";var e;const t=((e=globalThis.__sveltekit_pll7zu)==null?void 0:e.assets)??l;export{t as a,l as b};
