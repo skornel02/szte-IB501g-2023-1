4d9e233d065dba545e0fb8307a93ae3b37bf70c7
skornel02 <stefankornel02@gmail.com>
2023-11-11 18:03:17 +0000

    Deploying to gh-pages from  @ 2d61f55ae3fb099d7dbbf7c7d8c778be5112f9ff 🚀

---
+++ _app/immutable/chunks/navigation.745e576b.js
@@ -0,0 +1,1 @@
+import{h as o}from"./singletons.d141b581.js";const a=o("goto"),i=o("invalidate_all");export{a as g,i};
--- _app/immutable/chunks/navigation.f10c6a59.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./singletons.777a11eb.js";const a=o("goto"),i=o("invalidate_all");export{a as g,i};
